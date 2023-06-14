637ed89db57cf7c7c9f46b972dce06cf1f6dc977
Anas5006 <anassain13@gmail.com>
2023-06-14 22:13:11 +0530

    Fixed Bug

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,22 +1,8 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import EmptyState from '../components/EmptyState'
-import axios from 'axios'
-import { useAppSelector } from '../redux/hooks'
-
 
 const page = () => {
-
-  const { user} = useAppSelector((state) => state.user)
-  
-  useEffect(() => {
-    async function fetchConversation() {
-      await axios.get('/api/conversation').then((data) => {
-      })
-    }
-    if (user)
-      fetchConversation()
-  }, [user])
 
   return (
     <>
