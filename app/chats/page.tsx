c106023965aa43ae37a6b3c35780b23f5a729477
Anas5006 <anassain13@gmail.com>
2023-07-30 14:26:53 +0530

    Updated

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,24 +1,13 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import EmptyState from '../components/EmptyState'
-import { useAppDispatch, useAppSelector } from '../redux/hooks'
-import { setUserMode } from '../redux/user/slice'
 
 const page = () => {
-  const { mode } = useAppSelector((state) => state.user)
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    if (!localStorage.mode) {
-        localStorage.setItem("mode", "light")
-        dispatch(setUserMode("light"))
-    } else {
-        dispatch(setUserMode(localStorage.getItem("mode") || ""))
-    }
-  }, [dispatch])
+
   return (
     <>
       <div className={`hidden h-full md:pl-80 md:block`}>
-        {mode && <EmptyState mode={mode}/>}
+        <EmptyState />
       </div>
     </>
   )
